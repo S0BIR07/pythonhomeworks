f80f79fc970620f79ae2300fa223768b349de502
S0BIR07 <m.sobir07@gmail.com>
2025-02-23 20:51:42 +0500

    homework

---
+++ lesson-6/homework/zero_check_decorator.py
@@ -0,0 +1,12 @@
+def check(func):
+    def wrapper(a, b):
+        if b==0:
+            return "Denominator cannot be zero"
+        return func(a, b)
+    return wrapper
+@check
+def div(a, b):
+    return a / b
+a = int(input("Input the first number: "))
+b = int(input("Input the second number: "))
+print(div(a, b))
